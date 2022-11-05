d9dbfd4ed498e06b59f8dcd9dae0bcac30106bc1
Sammot <45080494+Sammot@users.noreply.github.com>
2022-11-05 16:10:49 +0000

    implemented tabset nb, started dashboard

--- web/js/pages/dashboard.js
+++ web/js/pages/dashboard.js
@@ -1,3 +1,9 @@
 "use strict"
 
-let mainNotebook = new TabsetNotebook(document.getElementById("main-notebook"))
+const links = document.querySelectorAll('link[rel="import"]');
+links.forEach((link) => {
+    let template = link.import.querySelector("template");
+    let clone = document.importNode(template.content, true);
+    let target = "dashboard";
+    document.getElementById(target).appendChild(clone);
+});
--- web/js/static/tabset-notebook.js
+++ web/js/static/tabset-notebook.js
@@ -3,22 +3,41 @@
 class TabsetNotebook {
     constructor(mainElement) {
         this.element = mainElement
+
         this.tablinks = this.element.querySelector('.tabset-links')
         this.tabcontent = this.element.querySelector('.tabset-contents')
 
-        const tablinks_list = Array.from(this.tablinks.children)
-        const tabcontent_list = Array.from(this.tabcontent.children)
+        this.tablinks_list = Array.from(this.tablinks.children)
+        this.tabcontent_list = Array.from(this.tabcontent.children)
 
-        tablinks_list.forEach(tab => {
+        //tab button event listener
+        this.tablinks_list.forEach(tab => {
             tab.addEventListener('click', () => {
-                tabcontent_list.forEach(tabInfo => {
+                //hide every other tab
+                this.tabcontent_list.forEach(tabInfo => {
                     tabInfo.classList.add('hidden')
                 })
-
-                console.log("click: " + tab.name + " " + this.tabcontent + " " + this.tabcontent.id)
-                console.log(this.tabcontent.querySelector('#' + tab.name))
+                //show the desired tab
                 this.tabcontent.querySelector('#' + tab.name).classList.remove("hidden")
             })
         })
+
+        //show the default tab
+        this.selectDefault()
+    }
+
+    selectDefault() {
+        //start out with all tabs hidden
+        this.tabcontent_list.forEach(tabInfo => {
+            tabInfo.classList.add('hidden')
+        })
+        //show the default tab
+        this.tablinks.querySelector('.tabset-link-default').click()
     }
 }
+
+//automatically set up all notebooks in the document
+var TabsetNotebooks = []
+const tabsetnotebooks = Array.from(document.getElementsByClassName('tabset-notebook')).forEach(notebook => {
+    TabsetNotebooks.push(new TabsetNotebook(notebook))
+})
