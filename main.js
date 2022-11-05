a8e99936ae5f290557c6fbc4c656ff9c43a5ea00
Sammot <45080494+Sammot@users.noreply.github.com>
2022-11-05 13:25:25 +0000

    Initial push

---
+++ main.js
@@ -0,0 +1,59 @@
+"use strict"
+
+const path = require('path')
+const { app, BrowserWindow } = require('electron')
+const log = require('electron-log');
+
+const NODE_ENV = 'development'
+
+log.transports.file.resolvePath = () => path.join(__dirname, 'mainlog.txt')
+Object.assign(console, log.functions)
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        minWidth: 400,
+        minHeight: 300,
+
+        titleBarStyle: 'hidden',
+        titleBarOverlay: {
+            color: '#b576f4',
+            symbolColor: '#ffffff',
+            height: 30
+        },
+
+        transparent: true,
+
+        autoHideMenuBar: true,
+
+        webPreferences: {
+            devTools: (NODE_ENV == "development") ? true : false,
+            preload: path.join(__dirname, 'preload.js')
+        },
+
+        show: false
+    })
+
+    win.once('ready-to-show', () => {
+      win.show()
+    })
+  
+    win.loadFile('web/index.html')
+}
+
+app.whenReady().then(() => {
+    createWindow()
+  
+    app.on('activate', () => {
+      if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+app.on('window-all-closed', () => {
+    try {
+        if (process.platform !== 'darwin') app.quit()
+    } catch (error) {
+        console.log(error)
+    }
+})
