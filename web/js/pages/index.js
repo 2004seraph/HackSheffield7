fd0848403ed8546bbc705d946cfd243c363befa1
Pawek <44177078+PawekPL@users.noreply.github.com>
2022-11-06 10:22:20 +0000

    menu fixes

--- web/js/pages/index.js
+++ web/js/pages/index.js
@@ -1,120 +1,130 @@
-"use strict"
+"use strict";
 
-let UserDataCache = {basicConfig: {}}
+let UserDataCache = { basicConfig: {} };
 
-let firstTimeSetupElement = document.getElementById("first-time-set-up")
+let firstTimeSetupElement = document.getElementById("first-time-set-up");
 
 let setup = new StagedConfig(firstTimeSetupElement, () => {
-    UserDataCache.basicConfig.completed = true
+  UserDataCache.basicConfig.completed = true;
 
-    window.api.send("saveUserData", UserDataCache)
-    //save state to file
+  window.api.send("saveUserData", UserDataCache);
+  //save state to file
 
-    gotoDashboard()
-})
+  gotoDashboard();
+});
 
 //check if state exists, if so, skip to dash
-window.api.sendInvoke('getUserData', null).then((data) => {
-    if (Object.hasOwn(data, "basicConfig")) {
-        if (data.basicConfig.completed) {
-            gotoDashboard()
-        } else {
-            UserDataCache = ProgramState.create()
-            firstTimeSetupElement.classList.remove("hidden")
-        }
+window.api.sendInvoke("getUserData", null).then((data) => {
+  if (Object.hasOwn(data, "basicConfig")) {
+    if (data.basicConfig.completed) {
+      gotoDashboard();
     } else {
-        UserDataCache = ProgramState.create()
-        firstTimeSetupElement.classList.remove("hidden")
+      UserDataCache = ProgramState.create();
+      firstTimeSetupElement.classList.remove("hidden");
     }
-})
+  } else {
+    UserDataCache = ProgramState.create();
+    firstTimeSetupElement.classList.remove("hidden");
+  }
+});
 
 function gotoDashboard() {
-    document.getElementById("dashboard-redirect").click()
+  document.getElementById("dashboard-redirect").click();
 }
 
 window.api.receive("transactionData", (event, data) => {
-    console.log(data)
-})
+  console.log(data);
+});
 
 function signIn() {
-    console.log("sign in")
+  console.log("sign in");
 
-    document.getElementById("loading-indicator").classList.remove("hidden")
+  document.getElementById("loading-indicator").classList.remove("hidden");
 
-    window.api.send('getUserPurchases', {id: document.getElementById("accountID").value})
-    //     console.log(data)
-    // })
+  window.api.send("getUserPurchases", {
+    id: document.getElementById("accountID").value,
+  });
+  document.getElementById("sign-in").classList.add("hidden");
+  document.getElementById("info").classList.remove("hidden");
 
-    // const headers = new Headers()
-    // headers.append("Content-Type", "application/json")
-    // headers.append("Authorization", "Bearer " + AUTHJWT)
-    // headers.append("version", "1.0")
+  //     console.log(data)
+  // })
 
-    // const myInit = {
-    //     method: 'GET',
-    //     headers: headers
-    // }
+  // const headers = new Headers()
+  // headers.append("Content-Type", "application/json")
+  // headers.append("Authorization", "Bearer " + AUTHJWT)
+  // headers.append("version", "1.0")
 
-    // const myRequest = new Request('https://sandbox.capitalone.co.uk/developer-services-platform-pr/api/data/transactions/accounts/' + document.getElementById("accountID").value + "/transactions")
-    // let status
-    // fetch(myRequest, myInit)
-    //     .then((response) => {
-    //         console.log("response")
-    //         console.log(response)
-    //         console.log(response.body)
+  // const myInit = {
+  //     method: 'GET',
+  //     headers: headers
+  // }
 
-    //         status = response.status
-            
-    //         const reader = response.body.getReader();
-    //         let x = new ReadableStream({
-    //             start(controller) {
-    //                 return pump();
-    //                 function pump() {
-    //                 return reader.read().then(({ done, value }) => {
-    //                     // When no more data needs to be consumed, close the stream
-    //                     if (done) {
-    //                     controller.close();
-    //                     return;
-    //                     }
-    //                     // Enqueue the next data chunk into our target stream
-    //                     controller.enqueue(value);
-    //                     return pump();
-    //                 });
-    //                 }
-    //             }
-    //         })
-    //     })
-    //     .then((stream) => new Response(stream))
-    //     .then((response) => {
-    //         response.blob().then((result) => {
-    //             console.log(result)
+  // const myRequest = new Request('https://sandbox.capitalone.co.uk/developer-services-platform-pr/api/data/transactions/accounts/' + document.getElementById("accountID").value + "/transactions")
+  // let status
+  // fetch(myRequest, myInit)
+  //     .then((response) => {
+  //         console.log("response")
+  //         console.log(response)
+  //         console.log(response.body)
 
-    //             if (status == 200) {
-    //                 const fr = new FileReader()
+  //         status = response.status
 
-    //                 fr.onload = (e) => {
-    //                     console.log(JSON.parse(e.target.result))
-    //                 }
+  //         const reader = response.body.getReader();
+  //         let x = new ReadableStream({
+  //             start(controller) {
+  //                 return pump();
+  //                 function pump() {
+  //                 return reader.read().then(({ done, value }) => {
+  //                     // When no more data needs to be consumed, close the stream
+  //                     if (done) {
+  //                     controller.close();
+  //                     return;
+  //                     }
+  //                     // Enqueue the next data chunk into our target stream
+  //                     controller.enqueue(value);
+  //                     return pump();
+  //                 });
+  //                 }
+  //             }
+  //         })
+  //     })
+  //     .then((stream) => new Response(stream))
+  //     .then((response) => {
+  //         response.blob().then((result) => {
+  //             console.log(result)
 
-    //                 fr.readAsText(result)
-    //                 next()
-    //             }
-    //         })
-    //     })
+  //             if (status == 200) {
+  //                 const fr = new FileReader()
+
+  //                 fr.onload = (e) => {
+  //                     console.log(JSON.parse(e.target.result))
+  //                 }
+
+  //                 fr.readAsText(result)
+  //                 next()
+  //             }
+  //         })
+  //     })
 }
 
 function next() {
-    switch (setup.stage) {
-        case 0:
-            UserDataCache.basicConfig.accountID = document.getElementById("accountID").value
-            break
-        case 1:
-            UserDataCache.basicConfig.businessName = document.getElementById("businessName").value
-            UserDataCache.basicConfig.financialYearEnd = document.getElementById("finyeardate").value
-            break
-        case 2:
-            break
-    }
+  switch (setup.stage) {
+    case 0:
+      UserDataCache.basicConfig.accountID =
+        document.getElementById("accountID").value;
+      document.getElementById("info").classList.add("hidden");
+      document.getElementById("prefs").classList.remove("hidden");
+      break;
+    case 1:
+      UserDataCache.basicConfig.businessName =
+        document.getElementById("businessName").value;
+      UserDataCache.basicConfig.financialYearEnd =
+        document.getElementById("finyeardate").value;
+      break;
+    case 2:
+      break;
+  }
 
-    setup.iterate()
+  setup.iterate();
 }
