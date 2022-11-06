1068583c88c332f4d734d4e2cfb2ea1d9c859009
jakub bala <63164657+JakubBala@users.noreply.github.com>
2022-11-06 13:04:17 +0000

    removed deadcode

--- web/js/pages/transaction.js
+++ web/js/pages/transaction.js
@@ -5,29 +5,9 @@ class Transaction {
 
     tag = null;
     overridedAmount = false;
-    valueSign = "pos";
 
     constructor(data) {
         this.rawTransactionData = data
-
-        if(data.amount > 0) {
-            this.valueSign = "pos";
-        } else {
-            this.valueSign = "neg";
-        }
-
-        this.html = `
-            <div class="transaction-div">
-                <div class="date-and-tag">
-                    <span class="tr-date">` + data.timestamp + `</span><span class="tr-tag">` + this.tag + `</span>
-                </div>
-                <div class="name-and-amount">
-                    <span class="tr-name">` + data.message + `</span><span class="tr-amount value-` + this.valueSign + `">£` + data.amount + `</span>
-                </div>
-                <div class="tr-reference">
-                    ` + data.message + `
-                </div>
-            </div>`;
     }
 
     /**
